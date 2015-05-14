a3e5749224312891f90c3600e6a3fec355d5f85f
Talshiar <domagoj.hrestak@gmail.com>
2015-05-14 17:30:21 +0200

    Start working

--- SystemRecommenderApp/Models/MyAppUserFriend.cs
+++ SystemRecommenderApp/Models/MyAppUserFriend.cs
@@ -7,11 +7,11 @@ namespace SystemRecommenderApp.Models
 {
     public class MyAppUserFriend
     {
-        public string Id { get; set; }
+        //public string Id { get; set; }
 
         public string Name { get; set; }
 
-        public string Gender { get; set; }
+        //public string Gender { get; set; }
 
         public string Link { get; set; }
 
