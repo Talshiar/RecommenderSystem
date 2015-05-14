2c7481df3fb89e82e991f2bedb2ecce31a9031df
Talshiar <domagoj.hrestak@gmail.com>
2015-05-14 17:34:24 +0200

    Revert "Half-done testing app for bdays"
    This reverts commit ebdf874f7e7579f8a7d3dfa163b7a7eac6a3baec.

--- SystemRecommenderApp/Models/MyAppUser.cs
+++ SystemRecommenderApp/Models/MyAppUser.cs
@@ -12,14 +12,14 @@ namespace SystemRecommenderApp.Models
         public string Name { get; set; }
         public string Email { get; set; }
 
-        //[JsonProperty("picture")] // This renames the property to picture.
-        //[FacebookFieldModifier("type(large)")] // This sets the picture size to large.
+        [JsonProperty("picture")] // This renames the property to picture.
+        [FacebookFieldModifier("type(large)")] // This sets the picture size to large.
         public FacebookConnection<FacebookPicture> ProfilePicture { get; set; }
 
-        //[FacebookFieldModifier("limit(8)")] // This sets the size of the friend list to 8, remove it to get all friends.
+        [FacebookFieldModifier("limit(8)")] // This sets the size of the friend list to 8, remove it to get all friends.
         public FacebookGroupConnection<MyAppUserFriend> Friends { get; set; }
 
-        //[FacebookFieldModifier("limit(16)")] // This sets the size of the photo list to 16, remove it to get all photos.
+        [FacebookFieldModifier("limit(16)")] // This sets the size of the photo list to 16, remove it to get all photos.
         public FacebookGroupConnection<FacebookPhoto> Photos { get; set; }
     }
 }
