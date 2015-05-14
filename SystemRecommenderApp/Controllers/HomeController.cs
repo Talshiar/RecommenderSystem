0081b0997831fc7660a5fd7db07a04ab9f105f92
Talshiar <domagoj.hrestak@gmail.com>
2015-05-14 22:57:17 +0200

    ya

--- SystemRecommenderApp/Controllers/HomeController.cs
+++ SystemRecommenderApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace SystemRecommenderApp.Controllers
 {
     public class HomeController : Controller
     {
-        [FacebookAuthorize("email", "user_photos")]
+        [FacebookAuthorize("email", "user_photos", "user_likes")]
         public async Task<ActionResult> Index(FacebookContext context)
         {
             if (ModelState.IsValid)
@@ -27,7 +27,7 @@ namespace SystemRecommenderApp.Controllers
                 dynamic me = client.Get("zuck");
                 string name = me.first_name;
                 return View(me);*/
-                //return View(user);
+                return View(user);
             }
 
             return View("Error");
