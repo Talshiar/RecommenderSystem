a8a91be5ce7968310554b5f95320eacd687ae62c
Talshiar <domagoj.hrestak@gmail.com>
2015-05-17 22:30:25 +0200

    aa

--- SystemRecommenderApp/Models/MyAppUserFriend.cs
+++ SystemRecommenderApp/Models/MyAppUserFriend.cs
@@ -9,6 +9,7 @@ namespace SystemRecommenderApp.Models
     {
         public string Name { get; set; }
         public string Link { get; set; }
+        public int FriendFactor { get; set; }
 
         [FacebookFieldModifier("height(100).width(100)")] // This sets the picture height and width to 100px.
         public FacebookConnection<FacebookPicture> Picture { get; set; }
