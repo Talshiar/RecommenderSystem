8627f055a28654811843695d819a5717728d6d9d
Talshiar <domagoj.hrestak@gmail.com>
2015-05-17 20:46:03 +0200

    work

--- SystemRecommenderApp/Controllers/HomeController.cs
+++ SystemRecommenderApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace SystemRecommenderApp.Controllers
 {
     public class HomeController : Controller
     {
-        [FacebookAuthorize("email", "user_photos", "user_likes")]
+        [FacebookAuthorize("email", "user_photos", "user_likes", "user_friends")]
         public async Task<ActionResult> Index(FacebookContext context)
         {
             if (ModelState.IsValid)
