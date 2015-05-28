beb665c17e11e0c3bfe95fa17c0bca758b96f209
Talshiar <domagoj.hrestak@gmail.com>
2015-05-28 21:21:00 +0200

    books

--- SystemRecommenderApp/Models/MyAppUser.cs
+++ SystemRecommenderApp/Models/MyAppUser.cs
@@ -51,7 +51,7 @@ namespace SystemRecommenderApp.Models
         [FacebookFieldModifier("limit(16)")]
         public FacebookGroupConnection<FacebookPhoto> Photos { get; set; }
         //public FacebookGroupConnection<Movie> Movies { get; set; }
-        //public FacebookGroupConnection<Book> Books { get; set; }
+        public FacebookGroupConnection<Book> Books { get; set; }
         ////public FacebookGroupConnection<TV> Television { get; set; }
         //public FacebookGroupConnection<Bands> Music { get; set; }
 
