db7ef32a2e4e2556eec93f8c8a25af0fa2b637db
Talshiar <domagoj.hrestak@gmail.com>
2015-06-29 13:06:15 +0200

    ayi

--- SystemRecommenderApp/Models/Bands.cs
+++ SystemRecommenderApp/Models/Bands.cs
@@ -10,6 +10,6 @@ namespace SystemRecommenderApp.Models
         public string Name { get; set; }
         public string Id { get; set; }
         public string Link { get; set; }
-        public string About { get; set; }
+        public string Description { get; set; }
     }
 }
